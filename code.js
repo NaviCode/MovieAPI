bbee87297e5579851ce113573138207e3c9fad0b
Fernando Sanchez <ext-fernando.sanchez@here.com>
2017-05-03 16:57:42 -0400

    Init Commit

---
+++ code.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $('#searchSubmit').click(function(){
+        $('#movieResults').empty();
+        var search = $('#movie').val();
+        var apiEndPoint = "https://www.omdbapi.com/?s="+search+"&r=json";
+        $.get(apiEndPoint,function(data,status){
+            var results = data['Search'];
+            for(var i in results){
+                console.log(results[i]['Title'])
+                $('#movieResults').append(results[i]['Title'] + '<br>');
+            }
+        })
+    })
+})
